2bcb8dda84d77e1323c81a92c60bc325e690dc07
sukkla01 <sukkla01@gmail.com>
2023-04-18 10:03:29 +0700

    xx

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
     font: 'Kanit',
-    BASE_URL: 'http://110.49.126.23:5005',
+    // BASE_URL: 'http://110.49.126.23:5005',
+    BASE_URL: 'http://110.164.59.133:5005',
     // BASE_URL: 'http://localhost:5005',
 }
 
